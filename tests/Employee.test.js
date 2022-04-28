3253d79bdba9d3140eadd98f5c926559ab379afd
cdfoye <cdfoye@gmail.com>
2022-04-28 00:24:18 -0600

    Employee test suite passed

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -10,4 +10,34 @@ describe('Employee', () => {
             expect(result.name).toEqual(name);
         })
     });
+
+    describe('getId', () => {
+        it("should return the id of the employee", () => {
+            const id = 1;
+
+            const result = new Employee('Tesla', id);
+
+            expect(result.id).toEqual(id);
+        })
+    });
+
+    describe('getEmail', () => {
+        it("should return the email of the employee", () => {
+            const email = 'email@email.com';
+
+            const result = new Employee('Tesla', 1, email);
+
+            expect(result.email).toEqual(email);
+        })
+    });
+
+    describe('getRole', () => {
+        it("should return the role of the employee", () => {
+            const role = 'Employee';
+
+            const result = new Employee('Tesla', 1, 'email@email.com');
+
+            expect(result.getRole()).toBe(role);
+        })
+    });
 });
